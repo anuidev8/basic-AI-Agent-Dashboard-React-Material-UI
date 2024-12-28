f33d60fee8b0befda98ce61760a4c953bae47570
anuidev8 <angelarrieta34@gmail.com>
2024-12-28 17:33:57 -0500

    feat: it has been added stats info

--- src/presentation/components/AgentStats.tsx
+++ src/presentation/components/AgentStats.tsx
@@ -1,16 +1,31 @@
 import {  Grid2, } from "@mui/material"
-import { FC } from "react"
+import { FC, useMemo } from "react"
 import { AgentStatsProps } from "./Agent/types"
 import { StatCard } from "./Agent/AgentStats/StatCard"
 import {Assessment, SmartToy, Speed} from '@mui/icons-material';
+
+import { AgentStatus } from "../../domain/AgentTypes";
 export const AgentStats:FC<AgentStatsProps> = ({agents}) =>{
+
+
+    const stats = useMemo(()=>{
+        if(!agents.length) return null;
+        const activeAgents = agents.filter(agent => agent.status === AgentStatus.Active)
+        const avgAccuracy = agents.reduce((sum,agent)=>sum + agent.accuracy,0) / agents.length
+        return{
+            total:agents.length ?? 0,
+            active:activeAgents.length ?? 0,
+            accuracy:avgAccuracy.toFixed(1) ?? 0,
+            performance:85
+        }
+    },[agents])
     return(
     <>
     <Grid2 container spacing={3} mb={4}>
         <Grid2 size={{xs:12, sm:6,md:3}} >
             <StatCard
                     title="Total Agents"
-                    value={12}
+                    value={stats?.total ?? 0 }
                     icon={<SmartToy />}
                     trend={{ value: 12, isPositive: true }}
             />
@@ -20,7 +35,7 @@ export const AgentStats:FC<AgentStatsProps> = ({agents}) =>{
         <Grid2 size={{xs:12, sm:6,md:3}} >
             <StatCard
                     title="Active  Agents"
-                    value={10}
+                    value={stats?.active ?? 0}
                     icon={<SmartToy />}
                     trend={{ value: 8, isPositive: true }}
             />
@@ -30,7 +45,7 @@ export const AgentStats:FC<AgentStatsProps> = ({agents}) =>{
         <Grid2 size={{xs:12, sm:6,md:3}} >
             <StatCard
              title="Average Accuracy"
-             value={'10%'}
+             value={stats?.accuracy ?? 0}
              icon={<Assessment />}
              trend={{ value: 5, isPositive: true }}
             />
@@ -40,7 +55,7 @@ export const AgentStats:FC<AgentStatsProps> = ({agents}) =>{
           <Grid2 size={{xs:12, sm:6,md:3}}>
        <StatCard
          title="Performance"
-         value={'10%'}
+         value={stats?.performance ?? 0}
          icon={<Speed />}
          trend={{ value: 3, isPositive: false }}
        />
